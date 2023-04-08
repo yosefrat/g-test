2c212c04c7dc1bdb7f074a32080a859646329618
yosefrat <yosefrat@protonmail.com>
2023-04-08 12:24:03 -0700

    Working example.

--- mysqlsproc.py
+++ mysqlsproc.py
@@ -1,2 +1,19 @@
-import mysql
+# That classic shit
 
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="secret-g",
+  port=3307,
+  database="test_houses"
+)
+
+# Cursor
+cur = mydb.cursor()
+
+# Retrieve
+result = cur.callproc('how_many_houses',['Jamille',0])
+
+print(result)
